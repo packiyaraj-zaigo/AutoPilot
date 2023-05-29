c01a4706c9065c00e4217fe01a0f6e9f53af4fc4
Packiyaraj <packiyaraj@zaigoinfotech.com>
2023-05-29 16:47:10 +0530

    changes

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -9,4 +9,5 @@ class AppConstants {
   static const String OTP = "OTP";
   static const String USER_ID = "USER_ID";
   static const String kGoogleApiKey  = "AIzaSyBSQft-_3nkzJK-2kFyuUKidm8kldWP1d8";
+
 }
