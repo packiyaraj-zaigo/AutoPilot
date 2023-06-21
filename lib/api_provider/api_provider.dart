6a87b350a2ae8c8e480e63a6377d06be27469e61
Hari <hariharan@zaigoinfotech.com>
2023-06-21 11:22:27 +0530

    vechile screen bloc and api call

--- lib/api_provider/api_provider.dart
+++ lib/api_provider/api_provider.dart
@@ -1,46 +1,38 @@
-
 import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-
 
 class ApiProvider {
   //DEV Server
 
-  static const BASE_URL = 'http://ec2-13-235-8-199.ap-south-1.compute.amazonaws.com/';
-
+  static const BASE_URL =
+      'http://ec2-13-235-8-199.ap-south-1.compute.amazonaws.com/';
 
   late Map<String, String> requestHeaders;
 
   getHeader(String token) {
     return requestHeaders = {
-     
       HttpHeaders.contentTypeHeader: 'application/json',
       HttpHeaders.authorizationHeader: 'Bearer $token',
     };
   }
 
-
-  Future<dynamic> createAccount(
-      String firstName,String lastName,String email,String password,String phoneNumber) async {
+  Future<dynamic> createAccount(String firstName, String lastName, String email,
+      String password, String phoneNumber) async {
     print("into provider");
-  
+
     //  LoadingFormModel? loadingFormModel;
     try {
-      var url = Uri.parse(
-       "${BASE_URL}api/register"
-           
-      );
+      var url = Uri.parse("${BASE_URL}api/register");
       var request = http.MultipartRequest("POST", url)
         ..fields['email'] = email
         ..fields['first_name'] = firstName
-        ..fields['last_name']=lastName
-        ..fields['emp_phone']=phoneNumber
-        ..fields['password']=password
-        ..fields['users']=json.encode([]);
-
-     
+        ..fields['last_name'] = lastName
+        ..fields['emp_phone'] = phoneNumber
+        ..fields['password'] = password
+        ..fields['users'] = json.encode([]);
 
       // request.headers.addAll(getHeader(token));
       var response = await request.send();
@@ -53,24 +45,15 @@ class ApiProvider {
     }
   }
 
-
-   Future<dynamic> login(
-      String email,String password) async {
+  Future<dynamic> login(String email, String password) async {
     print("into provider");
-  
+
     //  LoadingFormModel? loadingFormModel;
     try {
-      var url = Uri.parse(
-       "${BASE_URL}api/login"
-           
-      );
+      var url = Uri.parse("${BASE_URL}api/login");
       var request = http.MultipartRequest("POST", url)
         ..fields['email'] = email
-      
-        ..fields['password']=password;
-      
-
-     
+        ..fields['password'] = password;
 
       // request.headers.addAll(getHeader(token));
       var response = await request.send();
@@ -83,23 +66,15 @@ class ApiProvider {
     }
   }
 
-
-  Future<dynamic> getRevenueChartData(String token
-     ) async {
+  Future<dynamic> getRevenueChartData(String token) async {
     print("into provider");
-  
+
     //  LoadingFormModel? loadingFormModel;
     try {
-      var url = Uri.parse(
-       "${BASE_URL}api/dashboard"
-           
-      );
+      var url = Uri.parse("${BASE_URL}api/dashboard");
       var request = http.MultipartRequest("Get", url);
-      
 
-     
-
-       request.headers.addAll(getHeader(token));
+      request.headers.addAll(getHeader(token));
       var response = await request.send();
       inspect(response);
       print(response.statusCode.toString() + "provider status code");
@@ -110,22 +85,13 @@ class ApiProvider {
     }
   }
 
-
-  Future<dynamic> resetPasswordGetOtp(String email
-     ) async {
+  Future<dynamic> resetPasswordGetOtp(String email) async {
     print("into provider");
-  
+
     //  LoadingFormModel? loadingFormModel;
     try {
-      var url = Uri.parse(
-       "${BASE_URL}api/password/create"
-           
-      );
-      var request = http.MultipartRequest("POST", url)
-      ..fields['email']=email;
-      
-
-     
+      var url = Uri.parse("${BASE_URL}api/password/create");
+      var request = http.MultipartRequest("POST", url)..fields['email'] = email;
 
       // request.headers.addAll(getHeader(token));
       var response = await request.send();
@@ -138,25 +104,17 @@ class ApiProvider {
     }
   }
 
-
-
-  Future<dynamic> resetPasswordSendOtp(String email,String otp
-     ) async {
+  Future<dynamic> resetPasswordSendOtp(String email, String otp) async {
     print("into provider");
-  
+
     //  LoadingFormModel? loadingFormModel;
     try {
       var url = Uri.parse(
-       "${BASE_URL}api/password/find",
-       
-           
+        "${BASE_URL}api/password/find",
       );
       var request = http.MultipartRequest("GET", url)
-      ..fields['email']=email
-      ..fields['code']=otp;
-      
-
-     
+        ..fields['email'] = email
+        ..fields['code'] = otp;
 
       // request.headers.addAll(getHeader(token));
       var response = await request.send();
@@ -169,25 +127,17 @@ class ApiProvider {
     }
   }
 
-
-  Future<dynamic> getUserProfile(String token
-     ) async {
+  Future<dynamic> getUserProfile(String token) async {
     print("into provider");
-  
+
     //  LoadingFormModel? loadingFormModel;
     try {
       var url = Uri.parse(
-       "${BASE_URL}api/myprofile",
-       
-           
+        "${BASE_URL}api/myprofile",
       );
       var request = http.MultipartRequest("GET", url);
-     
-      
 
-     
-
-       request.headers.addAll(getHeader(token));
+      request.headers.addAll(getHeader(token));
       var response = await request.send();
       inspect(response);
       print(response.statusCode.toString() + "provider status code");
@@ -197,8 +147,6 @@ class ApiProvider {
       print(e.toString() + "provider error");
     }
   }
-
-
 
   Future<dynamic> getEmployees(String token, int page) async {
     try {
@@ -209,6 +157,64 @@ class ApiProvider {
       return response;
     } catch (e) {
       print(e.toString() + 'get employee error');
+    }
+  }
+
+  Future<dynamic> getVechile(String token) async {
+    print("into provider");
+
+    //  LoadingFormModel? loadingFormModel;
+    try {
+      var url = Uri.parse(
+        "${BASE_URL}api/vehicles",
+      );
+      var request = http.MultipartRequest("GET", url);
+
+      request.headers.addAll(getHeader(token));
+      var response = await request.send();
+      inspect(response);
+      print(response.statusCode.toString() + "provider status code");
+      print(response.request!.method + "provider response");
+      return http.Response.fromStream(response);
+    } catch (e) {
+      print(e.toString() + "provider error");
+    }
+  }
+
+  Future<dynamic> addVechile(
+    BuildContext context,
+    String token,
+    String email,
+    String year,
+    String model,
+    String submodel,
+    String engine,
+    String color,
+    String vinNumber,
+    String licNumber,
+    String type,
+    String make,
+  ) async {
+    print("eeeeeeeeeeeeeeeeeeeeeeeeeee$token");
+
+    //  LoadingFormModel? loadingFormModel;
+    try {
+      var url = Uri.parse("${BASE_URL}api/vehicles?customer_id=4&client_id=64");
+      var request = http.MultipartRequest("POST", url)
+        ..headers['Authorization'] = "Bearer $token"
+        ..fields['vehicle_type'] = type
+        ..fields['vehicle_year'] = year
+        ..fields['vehicle_make'] = make
+        ..fields['vehicle_model'] = model
+        ..fields['vehicle_color'] = color;
+      var response = await request.send();
+      inspect(response);
+      print("wwwwwwwwwwwwwwwwwwwwwwwwwwwwww${response.reasonPhrase}");
+      print(response.toString() + "provider status code");
+      print(response.toString() + "provider response");
+      return http.Response.fromStream(response);
+    } catch (e) {
+      print(e.toString() + "provider error");
     }
   }
 }
--- lib/api_provider/api_repository.dart
+++ lib/api_provider/api_repository.dart
@@ -1,36 +1,72 @@
+import 'package:flutter/material.dart';
+
 import 'api_provider.dart';
 
 class ApiRepository {
   final apiProvider = ApiProvider();
 
-   Future createAccount(String firstName,lastName,email,phoneNumber,password) {
-    return apiProvider.createAccount(firstName, lastName, email, password, phoneNumber);
+  Future createAccount(
+      String firstName, lastName, email, phoneNumber, password) {
+    return apiProvider.createAccount(
+        firstName, lastName, email, password, phoneNumber);
   }
 
-   Future login(String email,password) {
-    return apiProvider.login( email, password);
+  Future login(String email, password) {
+    return apiProvider.login(email, password);
   }
 
-
-   Future getRevenueChartData(dynamic token) {
+  Future getRevenueChartData(dynamic token) {
     return apiProvider.getRevenueChartData(token);
   }
 
-
-     Future resetPasswordGetOtp(String emailId) {
+  Future resetPasswordGetOtp(String emailId) {
     return apiProvider.resetPasswordGetOtp(emailId);
   }
 
-     Future resetPasswordSendOtp(String emailId,String otp) {
-    return apiProvider.resetPasswordSendOtp(emailId,otp);
+  Future resetPasswordSendOtp(String emailId, String otp) {
+    return apiProvider.resetPasswordSendOtp(emailId, otp);
   }
 
-       Future getUserProfile(String token) {
+  Future getUserProfile(String token) {
     return apiProvider.getUserProfile(token);
   }
 
   Future getEmployees(String token, int currentPage) {
     return apiProvider.getEmployees(token, currentPage);
+  }
+
+  Future getVechile(String token) {
+    return apiProvider.getVechile(token);
+  }
+
+  Future addVechile(
+    BuildContext context,
+    String token,
+    String email,
+    String year,
+    String model,
+    String submodel,
+    String engine,
+    String color,
+    String vinNumber,
+    String licNumber,
+    String type,
+    String make,
+  ) {
+    return apiProvider.addVechile(
+      context,
+      token,
+      email,
+      year,
+      model,
+      submodel,
+      engine,
+      color,
+      vinNumber,
+      licNumber,
+      type,
+      make,
+    );
   }
 }
 
