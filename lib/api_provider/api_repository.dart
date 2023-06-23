6c653b9e7bd20058a9873cd9af3a30ed9388bc74
Hari <hariharan@zaigoinfotech.com>
2023-06-23 19:09:52 +0530

    Search for vechicles

--- lib/api_provider/api_repository.dart
+++ lib/api_provider/api_repository.dart
@@ -74,8 +74,8 @@ class ApiRepository {
     return apiProvider.getAllRoles(token);
   }
 
-  Future getVechile(String token, int currentPage) {
-    return apiProvider.getVechile(token, currentPage);
+  Future getVechile(String token, int currentPage, String query) {
+    return apiProvider.getVechile(token, currentPage, query);
   }
 
   Future addVechile(
