09938d4bcea9de1b69f84326a9787f5a98394a3e
Aravind M S <aravind@zaigoinfotech.com>
2023-08-21 19:18:36 +0530

    bug fixes

--- lib/Screens/bottom_bar.dart
+++ lib/Screens/bottom_bar.dart
@@ -67,8 +67,8 @@ class _BottomBarScreenState extends State<BottomBarScreen>
 
     pages = [
       const DashBoardScreen(),
-      // WorkFlowScreen(tabController: workFlowTabController),
-      DummyScreen(name: "Work flow Screen"),
+      WorkFlowScreen(tabController: workFlowTabController),
+      // DummyScreen(name: "Work flow Screen"),
       // DummyScreen(name: "Calendar Screen"),
       const CalendarScreen(),
       EstimateScreen(
